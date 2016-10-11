592fa8837e40fac0b987ca659728d164674ac4eb
SoeKirile <SoeKirile@SoeKirile-PC>
2016-10-11 21:06:15 +0800

    remove log dir

---
+++ SoundEx/Log/Log4NetLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using log4net;
+using Prism.Logging;
+
+
+namespace SoundEx.Log
+{
+    public class Log4NetLogger : ILoggerFacade
+    {
+
+        #region Fields
+
+        // Member variables
+        private readonly ILog m_Logger = LogManager.GetLogger(typeof(Log4NetLogger));
+
+        #endregion
+
+        #region ILoggerFacade Members
+
+        /// <summary>
+        /// Writes a log message.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        /// <param name="category">The message category.</param>
+        /// <param name="priority">Not used by Log4Net; pass Priority.None.</param>
+        public void Log(string message, Category category, Priority priority)
+        {
+            switch (category)
+            {
+                case Category.Debug:
+                    m_Logger.Debug(message);
+                    break;
+                case Category.Warn:
+                    m_Logger.Warn(message);
+                    break;
+                case Category.Exception:
+                    m_Logger.Error(message);
+                    break;
+                case Category.Info:
+                    m_Logger.Info(message);
+                    break;
+            }
+        }
+
+
+
+        #endregion
+    }
+}
+
